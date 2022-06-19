b5685210e79fad91a299f46a71562def0cf8297b
Alhladkiy <Alhladkiy@gmail.com>
2022-06-19 18:33:33 +0300

    feat: add find iser to id

--- index.ts
+++ index.ts
@@ -9,7 +9,20 @@ const server = http.createServer((req: any, res: any): void => {
     if (req.url === '/api/users' && req.method === 'GET') {
         res.writeHead(200, {'Content-Type': 'application.json'});
         res.end(JSON.stringify(users));
-    } else {
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+        const id = req.url.split('/')[3];
+        const user = users.find((item) => item.id === id)
+        if (!user) {
+            res.writeHead(400, {'Content-Type': 'application.json'});
+            res.end(JSON.stringify({
+                message: 'Users not found'
+            }));
+        } else {
+            res.writeHead(200, {'Content-Type': 'application.json'});
+            res.end(JSON.stringify(user));
+        }
+    } 
+    else {
         res.writeHead(404, {'Content-Type': 'application.json'});
         res.end(JSON.stringify({
             message: 'Route not found' 
