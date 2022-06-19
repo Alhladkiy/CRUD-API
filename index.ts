c720d7f5b45c6a30ff54f8bf0e3653147b8518cd
Alhladkiy <Alhladkiy@gmail.com>
2022-06-19 17:53:54 +0300

    feat: add get all users

--- index.ts
+++ index.ts
@@ -1,4 +1,23 @@
-const sum = (a: number, b: number) => {
-    console.log( a + b)
-}
-sum(23423, 44534)
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const http = require('http');
+const port = process.env.PORT || 3000;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const users = require('./src/data/users')
+
+const server = http.createServer((req: any, res: any): void => {
+    if (req.url === '/api/users' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application.json'});
+        res.end(JSON.stringify(users));
+    } else {
+        res.writeHead(404, {'Content-Type': 'application.json'});
+        res.end(JSON.stringify({
+            message: 'Route not found' 
+        }));
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
+
